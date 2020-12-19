2aba2476c64bd50461c07ae832ea3d70564f3628
Abdullah-Asad <75277035+Abdullah-Asad@users.noreply.github.com>
2020-12-19 06:17:08 +0500

    Update prediction.py

--- prediction.py
+++ prediction.py
@@ -18,26 +18,26 @@ app = Flask(__name__)
 
 
 
-@app.route('/')
-def home_endpoint():
-    return 'Hello World!'
+# @app.route('/')
+# def home_endpoint():
+#     return 'Hello World!'
 
 
-@app.route('/predict', methods=['POST'])
-def get_prediction():
-    # Works only for a single sample
-    if request.method == 'POST':
-        print("1")
-        user_image = request.files["file"]
-        image = cv2.imdecode(np.frombuffer(user_image.read(), np.uint8), cv2.IMREAD_GRAYSCALE)
-        # image = cv2.imread('image.png', 0)
-        preparedImage = prepare(image)
+# @app.route('/predict', methods=['POST'])
+# def get_prediction():
+#     # Works only for a single sample
+#     if request.method == 'POST':
+#         print("1")
+#         user_image = request.files["file"]
+#         image = cv2.imdecode(np.frombuffer(user_image.read(), np.uint8), cv2.IMREAD_GRAYSCALE)
+#         # image = cv2.imread('image.png', 0)
+#         preparedImage = prepare(image)
 
-#         prediction = model.predict([preparedImage])
-#         print("6")
-#         prediction = list(prediction[0])
+# #         prediction = model.predict([preparedImage])
+# #         print("6")
+# #         prediction = list(prediction[0])
 
-        return [preparedImage]
+#         return [preparedImage]
 
 if __name__ == '__main__':
     
